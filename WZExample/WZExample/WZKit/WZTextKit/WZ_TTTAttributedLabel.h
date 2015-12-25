646845f4adbcac9f4461ee350d2f7303fdd8df2c
songbiwen <1099790014@qq.com>
2015-12-25 19:05:48 +0800

    + WZ_TTTAttributedLabel.h

---
+++ WZExample/WZExample/WZKit/WZTextKit/WZ_TTTAttributedLabel.h
@@ -0,0 +1,13 @@
+//
+//  WZ_TTTAttributedLabel.h
+//  WZExample
+//
+//  Created by songbiwen on 15/12/25.
+//  Copyright © 2015年 songbiwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WZ_TTTAttributedLabel : UILabel
+
+@end
---
+++ WZExample/WZExample/WZKit/WZTextKit/WZ_TTTAttributedLabel.m
@@ -0,0 +1,21 @@
+//
+//  WZ_TTTAttributedLabel.m
+//  WZExample
+//
+//  Created by songbiwen on 15/12/25.
+//  Copyright © 2015年 songbiwen. All rights reserved.
+//
+
+#import "WZ_TTTAttributedLabel.h"
+
+@implementation WZ_TTTAttributedLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
